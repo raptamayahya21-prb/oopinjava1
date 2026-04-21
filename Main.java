6a410c822af7ba399262e6fb7bedd75c9e3ef681
raptamayahya21-prb <raptamayahya21@gmail.com>
2026-04-21 14:35:30 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Instansiasi Object dari Class Buku
+        Buku bukuFavorit = new Buku("Laskar Pelangi", 75000);
+
+        // Memanggil method cetakInfo()
+        bukuFavorit.cetakInfo();
+    }
+}
