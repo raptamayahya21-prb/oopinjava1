8b26c1402f236c4dd37b6a4694b2558977e49b8b
raptamayahya21-prb <raptamayahya21@gmail.com>
2026-04-21 14:36:08 +0700

    Add files via upload

---
+++ MainMotor.java
@@ -0,0 +1,9 @@
+public class MainMotor {
+    public static void main(String[] args) {
+        // Instansiasi object Motor dengan nilai sesuai tugas
+        Motor motorSaya = new Motor("Honda", 150);
+
+        // Memanggil method start()
+        motorSaya.start();
+    }
+}
